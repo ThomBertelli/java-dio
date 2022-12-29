1f2613ec0190d517b68ce8f9b3da6dd8429ebe19
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-12-29 20:54:26 -0300

    ADD exercicio 1 arrays

---
+++ dio-backend/src/edu/thomas/loopsandarrays/arrays/exercicios/Exercicio01_OrdemInversa.java
@@ -0,0 +1,25 @@
+package edu.thomas.loopsandarrays.arrays.exercicios;
+
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+
+public class Exercicio01_OrdemInversa {
+
+	public static void main(String[] args) {
+		
+		int [] vetor = {1, 2, 3, 4, 5, 6};
+		
+		
+		for(int i = 5; i >= 0; i--) {
+			
+			System.out.println(vetor[i] + " ");
+			
+			
+		}
+
+	}
+
+}
+ 
---
+++ dio-backend/src/edu/thomas/loopsandarrays/arrays/exercicios/Exercicio02_Consoantes.java
@@ -0,0 +1,10 @@
+package edu.thomas.loopsandarrays.arrays.exercicios;
+
+public class Exercicio02_Consoantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
