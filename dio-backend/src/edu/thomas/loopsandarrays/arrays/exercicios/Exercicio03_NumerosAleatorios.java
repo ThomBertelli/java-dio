08d43476d96b1dc2b99be4c5744e39fed13d367d
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-12-29 23:17:57 -0300

    ADD exercicio 3 arrays

--- dio-backend/src/edu/thomas/loopsandarrays/arrays/exercicios/Exercicio03_NumerosAleatorios.java
+++ dio-backend/src/edu/thomas/loopsandarrays/arrays/exercicios/Exercicio03_NumerosAleatorios.java
@@ -1,5 +1,7 @@
 package edu.thomas.loopsandarrays.arrays.exercicios;
 
+import java.util.Random;
+
 /*
 Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
 Ao final mostre os números e seus sucessores.
@@ -8,7 +10,30 @@ public class Exercicio03_NumerosAleatorios {
 
 	public static void main(String[] args) {
 		
-		
+		 Random random = new Random();
+
+	        int[] numerosAleatorios = new int[20];
+
+	        for(int i = 0; i < numerosAleatorios.length; i++) {
+	            int numero = random.nextInt(100);
+	            numerosAleatorios[i] = numero;
+	        }
+
+	        System.out.print("Numeros Aleatorios: ");
+	        for (int numero : numerosAleatorios) {
+	            System.out.print(numero + " ");
+	        }
+
+	        System.out.print("\nAntecessor dos Numeros Aleatorios: ");
+	        for (int numero : numerosAleatorios) {
+	            System.out.print((numero-1) + " ");
+	        }
+
+	        System.out.print("\nSucessores dos Numeros Aleatorios: ");
+	        for (int numero : numerosAleatorios) {
+	            System.out.print((numero+1) + " ");
+	        }
+
 		
 		
 	}
