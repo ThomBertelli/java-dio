bb087b81682482aa1010c328ab5f1eca5e5727b6
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-12-29 21:31:08 -0300

    ADD exercicio 3 arrays

---
+++ dio-backend/src/edu/thomas/loopsandarrays/arrays/exercicios/Exercicio03_NumerosAleatorios.java
@@ -0,0 +1,16 @@
+package edu.thomas.loopsandarrays.arrays.exercicios;
+
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
+public class Exercicio03_NumerosAleatorios {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
