011ccc3cf5d4a37f6b03d3e82654a769f6e5d734
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-12-29 21:10:52 -0300

    ADD exercicio 2 arrays

--- dio-backend/src/edu/thomas/loopsandarrays/arrays/exercicios/Exercicio02_Consoantes.java
+++ dio-backend/src/edu/thomas/loopsandarrays/arrays/exercicios/Exercicio02_Consoantes.java
@@ -1,9 +1,34 @@
 package edu.thomas.loopsandarrays.arrays.exercicios;
+/*
+Faça um Programa que leia um vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
 
 public class Exercicio02_Consoantes {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	char[] vetor = {'a','b','c','d','e', 'f'};
+	
+	int quantidadeConsoantes = 0;
+	
+	for(char c : vetor) {
+		switch (c) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			
+			break;
+
+		default:
+			System.out.print(c + " ");
+			quantidadeConsoantes++;
+			break;
+		}
+	}
+	System.out.println(quantidadeConsoantes);
 
 	}
 
