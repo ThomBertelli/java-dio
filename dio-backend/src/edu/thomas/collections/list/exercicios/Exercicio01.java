4ce0551f7c6f70b0623f0d7ce85d40fdae3032d5
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-01-08 00:46:07 -0300

    ADD exercises's statements

--- dio-backend/src/edu/thomas/collections/list/exercicios/Exercicio01.java
+++ dio-backend/src/edu/thomas/collections/list/exercicios/Exercicio01.java
@@ -1,9 +1,17 @@
 package edu.thomas.collections.list.exercicios;
 
+/*Faça um programa que receba a temperatura média dos 6
+primeiros meses do ano e armazene-as em uma lista.
+
+Após isto, calcule a média semestral das temperaturas e
+mostre todas as temperaturas acima desta média, e em que
+mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2
+– Fevereiro e etc).*/
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Exercicio01 {
--- dio-backend/src/edu/thomas/collections/list/exercicios/Exercicio02.java
+++ dio-backend/src/edu/thomas/collections/list/exercicios/Exercicio02.java
@@ -1,5 +1,24 @@
 package edu.thomas.collections.list.exercicios;
 
+/*Utilizando listas, faça um programa que faça 5 perguntas para uma
+pessoa sobre um crime. As perguntas são:
+
+"Telefonou para a vítima?"
+
+"Esteve no local do crime?"
+
+"Mora perto da vítima?"
+
+"Devia para a vítima?"
+
+"Já trabalhou com a vítima?"
+
+Se a pessoa responder positivamente a 2 questões ela deve ser
+classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como
+"Assassina". Caso contrário, ela será classificado como "Inocente".*/
+
+
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
